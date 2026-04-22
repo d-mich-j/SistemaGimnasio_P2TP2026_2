1bc31b6a09d9a50dded43a31a45f1b6982714aec
d-mich-j <jimmida12@gmail.com>
2026-04-22 10:26:19 -0600

    se agregaron los tests

---
+++ SistemaGestionGimnasio.Test/Test1.cs
@@ -0,0 +1,29 @@
+﻿
+using SistemaGestionGimnasio.Modelo;
+
+namespace SistemaGestionGimnasio.Test 
+{
+    public class RutinaTests 
+    {
+        public void AgregarEjercicio_DebeAgregarALista() 
+        {
+            [Fact]
+            //Arange
+            Rutina rutina = new Rutina("Brazo", 120);
+            Ejercicio ejercicio1 = new Ejercicio ("Press", 4, 2, 60);
+            Ejercicio ejercicio2 = new Ejercicio("Fondo", 10, 3, 60);
+            Ejercicio ejercicio3 = new Ejercicio("Lagartijas", 15, 3, 60);
+
+            //Act
+
+            rutina.AgregarEjercicio(ejercicio1);
+            rutina.AgregarEjercicio(ejercicio2);
+            rutina.AgregarEjercicio(ejercicio3);
+
+            //Assert
+            Assert.Contains("Press", rutina.ObtenerEjercicios()[0].Nombre);
+            Assert.noempty
+        } 
+    }
+
+}
---
+++ SistemaGestionGimnasio/Gestores/GestorUsuarios.cs
@@ -0,0 +1,18 @@
+﻿using SistemaGestionGimnasio.Modelo;
+
+namespace SistemaGestionGimnasio.Gestores {
+    public class GestorUsuarios {
+        List<Usuario> usuarios;
+
+        public GestorUsuarios() {
+            usuarios = new List<Usuario>();
+        }
+
+        public void RegistrarUsuario(string nombre, int edad, string objetivo) {
+            usuarios.Add(new Usuario(nombre, edad, objetivo));
+        }
+        public  Usuario BuscarUsuario(string nombre) {
+            return usuarios.FirstOrDefault(u => u.Nombre == nombre);
+        }
+    }
+}
