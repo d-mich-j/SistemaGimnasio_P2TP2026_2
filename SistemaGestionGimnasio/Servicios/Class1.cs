38e972c100fba2543f576ea4f67b958366bcc817
d-mich-j <159874041+d-mich-j@users.noreply.github.com>
2026-04-29 08:25:32 -0600

    Update Class1.cs

--- SistemaGestionGimnasio/Servicios/Class1.cs
+++ SistemaGestionGimnasio/Servicios/Class1.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace SistemaGestionGimnasio.Servicios {
-    internal class Class1 {
+using SistemaGestionGimnasio.Modelo;
+
+namespace SistemaGestionGimnasio.Servicios
+{
+    public class AsignadorRutinas
+    {
+        public void AsignarRutinaaUsuario(Usuario usuario, Rutina rutina)
+        {
+            usuario.AsignarRutina(rutina);
+        }
+
+        public void AsignarUsuarioAEntrenador(Usuario usuario, Entrenador entrenador)
+        {
+            entrenador.AgregarUsuario(usuario);
+        }
     }
 }
