66e3380680eec36c4a5048615ef24a571642e2d4
d-mich-j <159874041+d-mich-j@users.noreply.github.com>
2026-04-29 08:18:00 -0600

    Update GestorUsuarios.cs

--- SistemaGestionGimnasio/Gestores/GestorUsuarios.cs
+++ SistemaGestionGimnasio/Gestores/GestorUsuarios.cs
@@ -1,17 +1,21 @@
-﻿using SistemaGestionGimnasio.Modelo;
-
-namespace SistemaGestionGimnasio.Gestores {
-    public class GestorUsuarios {
-        List<Usuario> usuarios;
-
-        public GestorUsuarios() {
+﻿using SistemaGestionGimnasio.Modelos;
+namespace SistemaGestionGimnasio.Gestores
+{
+    public class GestorUsuarios
+    {
+       List<Usuario> usuarios;
+        public GestorUsuarios()
+        {
             usuarios = new List<Usuario>();
         }
 
-        public void RegistrarUsuario(string nombre, int edad, string objetivo) {
-            usuarios.Add(new Usuario(nombre, edad, objetivo));
+        public void CrearUsuario(string nombre,int edad, string objetivo  )
+        {
+            usuarios.Add(new Usuario(nombre, edad, objetivo ));
         }
-        public  Usuario BuscarUsuario(string nombre) {
+
+        public Usuario BuscarUsuario(string nombre)
+        {
             return usuarios.FirstOrDefault(u => u.Nombre == nombre);
         }
     }
