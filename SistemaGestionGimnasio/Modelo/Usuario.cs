ebb550a6a2612ae995475345e512dfe6233f4efb
d-mich-j <159874041+d-mich-j@users.noreply.github.com>
2026-04-29 08:23:30 -0600

    Update Usuario.cs

--- SistemaGestionGimnasio/Modelo/Usuario.cs
+++ SistemaGestionGimnasio/Modelo/Usuario.cs
@@ -1,26 +1,24 @@
-﻿
-namespace SistemaGestionGimnasio.Modelo {
-
-
-    public class Usuario {
+﻿namespace SistemaGestionGimnasio.Modelo 
+ {
+    public class Usuario
+    {
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public string Objetivo { get; set; }
 
         public Rutina RutinaAsignada { get; set; }
 
-        public Usuario(string nombre, int edad, string objetivo) {
+        public Usuario(string nombre, int edad, string objetivo)
+        {
             Nombre = nombre;
             Edad = edad;
             Objetivo = objetivo;
+
         }
 
-
+        public void AsignarRutina(Rutina rutina)
+        {
+            RutinaAsignada = rutina;
+        }
     }
-
-    public void AsignarRutina (Rutina RUTINA) {
-
-        }
-
-}
-
+ }
